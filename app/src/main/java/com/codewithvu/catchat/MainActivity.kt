52158270ba5d154d6a3fc56b0ab85ba071f84c31
vuqt06 <vuqt06@gmail.com>
2022-08-07 22:04:22 -0400

    Create InboxFragment and HelpFragment - Import navigation library, create navigation graph and navigation host - Add menu ite m to navigate to HelpFragment

--- app/src/main/java/com/codewithvu/catchat/MainActivity.kt
+++ app/src/main/java/com/codewithvu/catchat/MainActivity.kt
@@ -2,7 +2,11 @@ package com.codewithvu.catchat
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.widget.Toolbar
+import androidx.navigation.findNavController
+import androidx.navigation.ui.onNavDestinationSelected
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,4 +15,15 @@ class MainActivity : AppCompatActivity() {
         val toolbar = findViewById<Toolbar>(R.id.toolbar)
         setSupportActionBar(toolbar)
     }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_toolbar, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment)
+        return item.onNavDestinationSelected(navController) || super.onOptionsItemSelected(item)
+    }
 }
+
